4621f8458df72d2ccf75a66e113241e90938cd5c
VColeman-games <coleman_valencia@columbusstate.edu>
2019-09-29 05:19:51 +0000

    Trying function again

--- includes/functions.php
+++ includes/functions.php
@@ -63,7 +63,7 @@ function page_content()
         $path = getcwd() . '/' . config('content_path') . '/404.php';
     }/*
     echo file_get_contents($path);*/
-    require config('config_path'). $path . '/'. $page . '.php';
+    require config($path);
 }
 
 /**
