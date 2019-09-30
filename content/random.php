1063c54d46e6778e14db3adb537e3afa6f820a09
VColeman-games <coleman_valencia@columbusstate.edu>
2019-09-30 04:07:22 +0000

    Added button

--- content/random.php
+++ content/random.php
@@ -1,2 +1,3 @@
 <div> <?php getComicTitleRand(); ?> </div>
 <div class="d-flex justify-content-center"><img src = <?php getComicImageRand(); ?>></div>
+<button onclick=<?php getComicImageRand(); ?>>New Comic</button>
