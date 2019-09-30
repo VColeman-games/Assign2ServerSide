747c7d0a21b9b2b493f8e7fa7aad9a46245cd1ca
VColeman-games <coleman_valencia@columbusstate.edu>
2019-09-30 14:40:16 +0000

    Adding ajax #5

--- content/random.php
+++ content/random.php
@@ -1,22 +1,5 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script>
-$(document).ready(function(){
-  $("button").click(function(){
-$.ajax(
-{
-type: 'post',  //whats your request type
-url: "random.php",  // whats your php file
-dataType:"json", // what type of data are you getting back
-success: function(data)
-{
-document.getElementByTagName("img").src=<?php getComicImageRand(); ?>
-//what are you going to do with what you get
-}
-});
-});
-}); // Ajax close
-</script>
+
 
 <div> <?php getComicTitleRand(); ?> </div>
 <div class="d-flex justify-content-center"><img src = <?php getComicImageRand(); ?>></div>
-<button >New Comic</button>
+<button onClick="window.location.reload();" >New Comic</button>
