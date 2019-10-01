2fc177a8243badbea6f151b31e950a773cceb87f
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-01 02:52:54 +0000

    Trying ajax

--- content/random.php
+++ content/random.php
@@ -1,23 +1,25 @@
 <script >
-            function ChangeComic(){
+            $('#change').click(function(){
                  //location.reload();
 
                 $.ajax({
 
-                    type: "GET",
-                    url: "file:///workspace/Assign2ServerSide/includes/functions.php",
-                    data: {'link': ""},
-                    dataType: "html",
+                    type: "post",
+                    url: "/includes/functions.php",
+                    data: {'random': true},
+                    dataType: "HTML",
                     success: function (data) {
-                        alert(data);
+                        $('#randomComic').html(data);
                     },
                     error: function (result) {
                         alert("Error");
                     }
                 });
+                return false;
             }
         </script>
-
+<div id="randomComic">
 <div> <?php getComicTitleRand(); ?> </div>
 <div class="d-flex justify-content-center"><img src = <?php getComicImageRand(); ?>></div>
+</div>
 <button onClick="ChangeComic()" >New Comic</button>
