ef9250b128e0832ce693ed03681dc7f4b43ba762
VColeman-games <coleman_valencia@columbusstate.edu>
2019-10-06 03:44:56 +0000

    testig ajax with isset #4

--- includes/functions.php
+++ includes/functions.php
@@ -101,6 +101,7 @@ echo '<div class="d-flex justify-content-center"> <img src = ' . $response["img"
 
 /**Function for getting the comic title and date for random comic for random comic */
 function getComicRand(){
+
 $randNum = rand(1,2208);
 $url = 'https://xkcd.com/'. $randNum. '/'.'info.0.json';
 /**dont change
@@ -123,7 +124,7 @@ echo '<br>';
 echo '<h4>' . $response["month"] . '/' . $response["day"] . '/'. $response["year"] . '</h4>';
 echo '<div class="d-flex justify-content-center"> <img src = ' . $response["img"] .'></div>';
 }
-$random = true;
+$random = false ;
 if(isset($random)){
     getComicRand();
 }
